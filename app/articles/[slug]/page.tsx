eb617a0997fb66b011d74109b2287f103ee85883
hironaga-1 <oretachisanta410@gmail.com>
2023-07-21 16:37:46 +0900

    modify metadata

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -5,7 +5,7 @@ import ArticleContent from "./ArticleContent";
 import Comments from "./Comments";
 import { Heading } from "../../common/components";
 import LoadingComments from "./LoadingComments";
-import { Metadata, ResolvingMetadata } from "next";
+import type { Metadata, ResovlingMetadata } from "next";
 
 const getArticle = async (slug: string) => {
   const res = await fetch(`http://localhost:3000/api/articles/${slug}`, {
